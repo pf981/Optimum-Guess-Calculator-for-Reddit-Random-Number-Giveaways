7a994d1db04a1e4609c7253bcd13ffa563d48e08
Paul Foster <paulfstr@gmail.com>
2016-04-05 01:04:22 +1000

    Remove unneeded code

--- reddit_number_extractor.R
+++ reddit_number_extractor.R
@@ -10,10 +10,8 @@ if (!require('pacman'))
     install.packages('pacman')
 
 pacman::p_load('ggplot2',
-               # 'plotrix',
                'RedditExtractoR',
                'tidyr')
-# For deeper text mining, use tm package
 
 
 MIN_GUESS <- 0
@@ -53,7 +51,7 @@ guess_df$is_guessed <- guess_df$number %in% guessed_numbers
 ## Plot the Density of the Guesses ####
 
 
-smoothed <- density(guessed_numbers, adjust=0.1)
+smoothed <- density(guessed_numbers, adjust=0.1) # You may need to tune the adjust parameter
 smoothed <- data.frame(x=smoothed$x, y=smoothed$y)
 
 
@@ -67,60 +65,30 @@ ggplot(smoothed, aes(x=x, y=1, fill=y)) +
     geom_tile()
 
 
-# # Red to green
-# ggplot(smoothed, aes(x=x, y=1, fill=y)) +
-#     labs(title='Density of Guessed Numbers',
-#          y='',
-#          x='Guessed Number') +
-#     theme(axis.ticks=element_blank(), axis.text.y=element_blank()) +
-#     xlim(MIN_GUESS, MAX_GUESS) +
-#     scale_fill_gradient(low='green', high='red', name='Density') +
-#     geom_tile()
-
-
-# gap.plot(21:30,rnorm(10)+40,gap=c(8,16,25,35),add=TRUE,
-#   lty=rep(3,10),col=rep(4,10),type="l")
-
 
 
 ## Find the Bigest Gap in the Guesses ####
-
-
-# with(rle(guess_df$is_guessed), {
-#   ok <- !is.na(values)
-#   ends <- cumsum(lengths)[ok]
-#   starts <- ends - lengths[ok] + 1
-#   cbind(starts, ends)
-# })
-
-
 
 
 run_lengths <- rle(guess_df$is_guessed) %>%
     unclass %>%
     data.frame
 
-
+# Find the biggest gap in the guessses. Gaps are simply runs of FALSE.
 only_false_lengths <- ifelse(!run_lengths$values, run_lengths$lengths, 0)
 longest_false_run_index <- which.max(only_false_lengths)
 longest_false_run_length <- only_false_lengths[longest_false_run_index]
 
-
-cumsum(run_lengths$lengths)
-
+# Determine the beginning and end of the gap
 start_of_longest_gap <- cumsum(run_lengths$lengths)[longest_false_run_index-1]
 end_of_longest_gap <- start_of_longest_gap + longest_false_run_length
 
+# Determine the middle of the gap
 best_guess <- (start_of_longest_gap + end_of_longest_gap - 1) %/% 2
 
 
-message('Best guess is ', best_guess, ' which covers ', longest_false_run_length %/% 2, ' numbers.')
+message('The best new guess that can be made is ', best_guess, ' which covers ', longest_false_run_length %/% 2, ' numbers.')
 
-# run_lengths$lengths[run_lengths$values]
-#
-#
-#
-# run_lengths2 <-
 
 
 
