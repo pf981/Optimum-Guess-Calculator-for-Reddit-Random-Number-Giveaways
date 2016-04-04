638e5b46bc5596760fae45649814154eaf48c1ae
Paul Foster <paulfstr@gmail.com>
2016-04-05 00:56:45 +1000

    Determine best guess

--- reddit_number_extractor.R
+++ reddit_number_extractor.R
@@ -10,6 +10,7 @@ if (!require('pacman'))
     install.packages('pacman')
 
 pacman::p_load('ggplot2',
+               # 'plotrix',
                'RedditExtractoR',
                'tidyr')
 # For deeper text mining, use tm package
@@ -49,60 +50,77 @@ guess_df$is_guessed <- guess_df$number %in% guessed_numbers
 
 
 
-# smoothed <- density(guessed_numbers, adjust = 1)
+## Plot the Density of the Guesses ####
+
+
 smoothed <- density(guessed_numbers, adjust=0.1)
 smoothed <- data.frame(x=smoothed$x, y=smoothed$y)
-# smoothed <- density(guessed_numbers, adjust = 1) %>%
-#     select(x, y) %>%
-#     data.frame
-
-# smoothed$x
-# smoothed$y
 
 
 ggplot(smoothed, aes(x=x, y=1, fill=y)) +
+    labs(title='Density of Guessed Numbers',
+         y='',
+         x='Guessed Number') +
+    theme(axis.ticks=element_blank(), axis.text.y=element_blank()) +
     xlim(MIN_GUESS, MAX_GUESS) +
+    scale_fill_continuous(name='Density') +
     geom_tile()
 
 
-ggplot(guess_df, aes(x=number, y=1, fill=is_guessed)) + geom_tile()
+# # Red to green
+# ggplot(smoothed, aes(x=x, y=1, fill=y)) +
+#     labs(title='Density of Guessed Numbers',
+#          y='',
+#          x='Guessed Number') +
+#     theme(axis.ticks=element_blank(), axis.text.y=element_blank()) +
+#     xlim(MIN_GUESS, MAX_GUESS) +
+#     scale_fill_gradient(low='green', high='red', name='Density') +
+#     geom_tile()
+
+
+# gap.plot(21:30,rnorm(10)+40,gap=c(8,16,25,35),add=TRUE,
+#   lty=rep(3,10),col=rep(4,10),type="l")
+
+
+
+## Find the Bigest Gap in the Guesses ####
+
+
+# with(rle(guess_df$is_guessed), {
+#   ok <- !is.na(values)
+#   ends <- cumsum(lengths)[ok]
+#   starts <- ends - lengths[ok] + 1
+#   cbind(starts, ends)
+# })
 
 
 
 
-
-ggplot(guess_df, aes(x=number, y=is_guessed)) +
-    geom_histogram()
-
-
-
-ggplot(guess_df, aes(x=number, y=is_guessed)) +
-    geom_bar(stat='identity')
+run_lengths <- rle(guess_df$is_guessed) %>%
+    unclass %>%
+    data.frame
 
 
-ggplot(guess_df, aes(x=number, y=is_guessed)) +
-    geom_density()
+only_false_lengths <- ifelse(!run_lengths$values, run_lengths$lengths, 0)
+longest_false_run_index <- which.max(only_false_lengths)
+longest_false_run_length <- only_false_lengths[longest_false_run_index]
 
 
+cumsum(run_lengths$lengths)
 
-ggplot(guess_df, aes(x=number, y=1, fill=is_guessed)) + geom_density(adjust=0.1)
+start_of_longest_gap <- cumsum(run_lengths$lengths)[longest_false_run_index-1]
+end_of_longest_gap <- start_of_longest_gap + longest_false_run_length
 
-# ggplot(guess_df, aes(x=number, y=is_guessed)) + geom_tile()
-
-
-
-
+best_guess <- as.integer((start_of_longest_gap + end_of_longest_gap - 1) / 2)
 
 
+message('Best guess is ', best_guess, ' which covers ', '', ' numbers.')
 
-
-
-
-
-
-
-
-
+# run_lengths$lengths[run_lengths$values]
+#
+#
+#
+# run_lengths2 <-
 
 
 
